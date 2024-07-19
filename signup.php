d1b033c50e4b11a236e9462ae23383b60a66e62c
Monica Plata <monica.plataricaurte@academia.unimeta.edu.co>
2024-07-19 13:14:08 -0500

    Se corrigue mensaje de éxito.

--- signup.php
+++ signup.php
@@ -44,7 +44,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    Ya existe un usuario con este email.
+                Tu cuenta ha sido creada exitosamente. Ahora puedes iniciar sesión.
                 </div>
             </div>
         </div>
